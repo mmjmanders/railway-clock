9d671b4cfb0fea41e67abb968ef53b26a2b0737e
Mark Manders <mmjmanders@gmail.com>
2025-10-02 14:44:26 +0200

    Added clock hands

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { default as HourHand } from './HourHand.svelte';
+	import { default as MinuteHand } from './MinuteHand.svelte';
+	import { default as SecondHand } from './SecondHand.svelte';
+
 	const { hours, minutes, seconds }: { hours: number; minutes: number; seconds: number } = $props();
 	const hourMarkers = Array(12)
 		.fill(0)
@@ -13,15 +17,27 @@
 	<defs>
 		<path id="hour-marker" class="fill-current" d="M -15,-500 l 30,0 l 0,100 l -30,0 z" />
 		<path id="minute-marker" class="fill-current" d="M -7.5,-500 l 15,0 l 0,40 l -15,0 z" />
+		<path id="hour-hand" class="fill-current" d="M -20,-350 l 40,0 l 0,460 l -40,0 z" />
+		<path id="minute-hand" class="fill-current" d="M -20,-490 l 40,0 l 0,600 l -40,0 z" />
+		<g id="second-hand" class="text-red-600 dark:text-red-800">
+			<circle class="fill-current" cx="0" cy="0" r="10" />
+			<path class="fill-current" d="M -5,-360 l 10,0 l 0,480 l -10,0 z" />
+			<circle class="fill-current" cx="0" cy="-320" r="25" />
+		</g>
 	</defs>
 	<g id="hour-markers" class="text-neutral-950 dark:text-neutral-50">
-		{#each hourMarkers as marker}
+		{#each hourMarkers as marker (marker)}
 			<use href="#hour-marker" transform="rotate({marker * (360 / 12)})" />
 		{/each}
 	</g>
 	<g id="minute-markers" class="text-neutral-950 dark:text-neutral-50">
-		{#each minuteMarkers as marker}
+		{#each minuteMarkers as marker (marker)}
 			<use href="#minute-marker" transform="rotate({marker * (360 / 60)})" />
 		{/each}
+	</g>
+	<g id="hands" class="text-neutral-950 dark:text-neutral-50">
+		<HourHand {hours} {minutes} />
+		<MinuteHand {minutes} />
+		<SecondHand {seconds} />
 	</g>
 </svg>
