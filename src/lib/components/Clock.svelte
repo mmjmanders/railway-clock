eace43db2bc6c0765d1823bc17fe83dcba40103a
Mark Manders <mmjmanders@gmail.com>
2025-10-16 11:57:14 +0200

    Removed FontAwesome

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -54,11 +54,8 @@
 			text-anchor="middle"
 			class="font-[DM_Serif_Display] text-5xl"
 		>
-			<tspan class="fill-neutral-950 font-['Font_Awesome_7_Free'] dark:fill-neutral-50"
-				>&#xf133;</tspan
-			><tspan class="fill-neutral-950 dark:fill-neutral-50">&nbsp;{dayOfWeek}&nbsp;</tspan><tspan
-				class="fill-red-600 dark:fill-red-800">{day}</tspan
-			>
+			<tspan class="fill-neutral-950 dark:fill-neutral-50">{dayOfWeek}</tspan>
+			<tspan class="fill-red-600 dark:fill-red-800">{day}</tspan>
 		</text>
 	</g>
 	<g id="hash">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import '@fontsource/dm-serif-display';
 	import '@fontsource-variable/roboto-mono';
-	import '@fortawesome/fontawesome-free/css/regular.css';
 
 	import '../app.css';
 
