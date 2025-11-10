192a7588cb65e458c1a197ddf0c13a829da18a17
Mark Manders <mmjmanders@gmail.com>
2025-11-10 13:14:13 +0100

    Updated refresh rate

--- src/lib/components/SecondHand.svelte
+++ src/lib/components/SecondHand.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	const { seconds }: { seconds: number } = $props();
-	const rotation = $derived(seconds * (360 / 60));
+	const { seconds, milliseconds }: { seconds: number; milliseconds: number } = $props();
+	const rotation = $derived((seconds * 1000 + milliseconds) * (360 / (60 * 1000)));
 </script>
 
 <use href="#second-hand" transform="rotate({rotation})" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,17 @@
 	const hours = $derived(date.getHours() % 12);
 	const minutes = $derived(date.getMinutes());
 	const seconds = $derived(date.getSeconds());
+	const milliseconds = $derived(date.getMilliseconds());
 
 	$effect(() => {
 		const interval = setInterval(() => {
 			date = new Date();
-		}, 1000);
+		}, 1000 / 50);
 
 		return () => clearInterval(interval);
 	});
 </script>
 
 <div class="mx-auto h-[100vmin] w-[100vmin] p-4">
-	<Clock {hours} {minutes} {seconds} {date} />
+	<Clock {hours} {minutes} {seconds} {milliseconds} {date} />
 </div>
