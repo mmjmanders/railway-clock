a37e039ac243da183d3ecfb94563917958547be2
Mark Manders <mmjmanders@gmail.com>
2025-11-14 14:10:52 +0100

    Preload fonts (#59)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
 	import '@fontsource/dm-serif-display';
-	import '@fontsource-variable/roboto-mono';
+	import '@fontsource/roboto-mono';
+
+	import dmSerifDisplayWoff2 from '@fontsource/dm-serif-display/files/dm-serif-display-latin-400-normal.woff2?url';
+	import robotoMonoWoff2 from '@fontsource/roboto-mono/files/roboto-mono-latin-400-normal.woff2?url';
 
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link
+		rel="preload"
+		as="font"
+		type="font/woff2"
+		href={dmSerifDisplayWoff2}
+		crossorigin="anonymous"
+	/>
+	<link rel="preload" as="font" type="font/woff2" href={robotoMonoWoff2} crossorigin="anonymous" />
+</svelte:head>
 
 {@render children()}
